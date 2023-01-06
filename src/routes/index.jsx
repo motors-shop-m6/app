b6b28c0815381faf215a47ebb9b2b01c29e4cae2
Jhon Muller <jmuller.jhon80@gmail.com>
2023-01-06 00:23:02 -0400

    feat: desenvolvimento da rota product em mobile first, falta apenas os comentários.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Admin from "../pages/Admin";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
+import Product from "../pages/Product";
 
 function AppRoutes() {
   return (
@@ -9,7 +10,7 @@ function AppRoutes() {
       <Route path="/" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" /*element=<></>*/ />
-      <Route path="/product" /*element=<></>*/ />
+      <Route path="/product" element={<Product/>} />
       <Route path="/admin" element={<Admin />} />
     </Routes>
   );

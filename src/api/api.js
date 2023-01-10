19b2eb433195db9a2fbc78a631fc4d7e2c98d049
CARLA GRAZIELLE OLIVEIRA SUZART <carlagosuzart@gmail.com>
2023-01-10 12:29:55 -0300

    feat: pagina de cadastro

--- src/api/api.js
+++ src/api/api.js
@@ -6,3 +6,22 @@ export const api = axios.create({
   baseURL: BASE_URL,
   timeout: 50000,
 });
+
+export const registerUser = (data) =>
+  axios({
+    url: `${BASE_URL}/user`,
+    method: "POST",
+    data: data,
+  })
+    .then((success) => {
+      toast.success("Conta criada com sucesso!");
+      return success;
+    })
+    .catch((err) => {
+      if (err.response.data === "Email already exists") {
+        toast.error("Email já cadastrado, tente outro!");
+      } else {
+        toast.error("Ocorreu algum erro. Tente novamente mais tarde");
+      }
+      return err;
+    });
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 function AppRoutes(props) {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/register" /*element=<></>*/ />
+      <Route path="/register" element={<Register />} />
       <Route path="/product" /*element=<></>*/ />
     </Routes>
   );
